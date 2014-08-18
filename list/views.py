9c1902dae8ece2d7ea80b3005529b079bfdf04fa
mcrisan <crisan_mariusvlad@yahoo.com>
2014-08-18 15:59:07 +0300

    creating home page form and to add items in the list

--- list/views.py
+++ list/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
+    if request.method == 'POST':
+        item = request.POST.get('item_text', '')
+        return render(request, 'home.html', {'new_item_text' : item})
     return render(request, 'home.html')
