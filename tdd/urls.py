cb245b07721c1e8c1d5b94de2dc7ba21bbc525ef
mcrisan <crisan_mariusvlad@yahoo.com>
2014-08-19 11:41:23 +0300

    creating new list via post

--- tdd/urls.py
+++ tdd/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
     url(r'^lists/the-only-list-in-the-world/$', 'list.views.view_list',
         name='view_list'
     ),
+    url(r'^lists/new$', 'list.views.new_list', name='new_list'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
