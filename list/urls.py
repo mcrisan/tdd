087500b7df74102e89b4bd51cf7959005b16c047
mcrisan <crisan_mariusvlad@yahoo.com>
2014-08-20 12:43:01 +0300

    refactored list view to handle new items

--- list/urls.py
+++ list/urls.py
@@ -4,8 +4,5 @@ urlpatterns = patterns('',
     url(r'^(?P<list_id>\d+)/$', 'list.views.view_list',
         name='view_list'
     ),
-    url(r'^(?P<list_id>\d+)/add_item$', 'list.views.add_item',
-        name='view_list'
-    ),
     url(r'^new$', 'list.views.new_list', name='new_list'),
 )
