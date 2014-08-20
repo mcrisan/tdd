03da702c3a78aa5a4beba46fa79129dcc842253e
mcrisan <crisan_mariusvlad@yahoo.com>
2014-08-20 10:57:08 +0300

    Moved Fts into their own individual files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,28 +1,15 @@
-import unittest
-
-from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(LiveServerTestCase):  
+from .base import FunctionalTest
 
-    def setUp(self):  
-        self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
 
-    def tearDown(self):  
-        self.browser.quit()
-        
+class NewVisitorTest(FunctionalTest): 
     
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
     def test_can_start_a_list_and_retrieve_it_later(self):  
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -68,7 +55,7 @@ class NewVisitorTest(LiveServerTestCase):
         
         # Francis visits the home page.  There is no sign of Edith's
         # list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -90,30 +77,3 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
         
         # Satisfied, they both go back to sleep
-        
-    def test_layout_and_styling(self):
-            # Edith goes to the home page
-            self.browser.get(self.live_server_url)
-            self.browser.set_window_size(1024, 768)
-    
-            # She notices the input box is nicely centered
-            inputbox = self.browser.find_element_by_id('id_new_item')
-            self.assertAlmostEqual(
-                inputbox.location['x'] + inputbox.size['width'] / 2,
-                512,
-                delta=5
-            )
-            
-            # She starts a new list and sees the input is nicely
-            # centered there too
-            inputbox.send_keys('testing\n')
-            inputbox = self.browser.find_element_by_id('id_new_item')
-            self.assertAlmostEqual(
-                inputbox.location['x'] + inputbox.size['width'] / 2,
-                512,
-                delta=5
-            )
-
-
-if __name__ == '__main__':  
-    unittest.main()  
