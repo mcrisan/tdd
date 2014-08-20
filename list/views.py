efc48085df880eb8bee57122b78e6c037ea79b51
mcrisan <crisan_mariusvlad@yahoo.com>
2014-08-20 16:43:21 +0300

    form duplicate list item

--- list/views.py
+++ list/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
-from django.core.exceptions import ValidationError
 
-from list.models import Item, List
+from list.models import List
 from list.forms import ItemForm
 
 def home_page(request):
@@ -14,7 +12,7 @@ def view_list(request, list_id):
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)            
     return render(request, 'list.html', {'list': list_, 'form': form})
 
@@ -22,7 +20,7 @@ def new_list(request):
     form = ItemForm(data=request.POST) #1
     if form.is_valid(): #2
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'home.html', {"form": form})
