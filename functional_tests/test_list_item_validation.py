61cd7b95cdb32705f0964d48147b6fc0ca9a5276
mcrisan <crisan_mariusvlad@yahoo.com>
2014-08-21 11:42:22 +0300

    ft for input error now woks

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -53,11 +53,25 @@ class ItemValidationTest(FunctionalTest):
         self.browser.get(self.server_url)
         self.get_item_input_box().send_keys('\n')
         error = self.get_error_element()
-        self.assertTrue(error.is_displayed()) #1
+        self.assertTrue(error.is_displayed()) 
     
         # She starts typing in the input box to clear the error
         self.get_item_input_box().send_keys('a')
     
         # She is pleased to see that the error message disappears
         error = self.get_error_element()
-        self.assertFalse(error.is_displayed()) #2
+        self.assertFalse(error.is_displayed()) 
+        
+    def test_error_messages_are_cleared_on_click(self):
+        # Edith starts a new list in a way that causes a validation error:
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed()) 
+    
+        # She starts typing in the input box to clear the error
+        self.get_item_input_box().click()
+    
+        # She is pleased to see that the error message disappears
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed()) 
