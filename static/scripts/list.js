8e48d85f1f5c2368fec7e0cd3a11746e3d2723ee
mcrisan <crisan_mariusvlad@yahoo.com>
2014-08-21 14:39:38 +0300

    refactored static files

--- static/scripts/list.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function ($) {
-    $('#id_text').on('keypress', function () {
-        $('.has-error').hide();
-    });
-    
-    $('#id_text').on('click', function () {
-        $('.has-error').hide();
-    });
-});
